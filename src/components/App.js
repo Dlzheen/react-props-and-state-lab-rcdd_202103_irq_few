910d40c74b534c75c21987488ab6e689eebff297
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-06-23 21:14:44 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,48 @@
 import React from 'react';
-import Filters from './.js';
-import PetBrowser from './PetBrowser.js';
+import Filters from './Filters';
+import PetBrowser from './PetBrowser';
 class App extends React.Component {
       constructor() {
-            super();
-          this.state = {
-            pets: [],
-          filters: {
-          type: 'all'
-        }
-     };
-    }
-   fetchPets = () => {
-    let endpoint = '/api/pets';
-    if (this.state.filters.type !== 'all') {
-      endpoint += `?type=${this.state.filters.type}`;
+                      super();
+                          this.state = {
+                                 pets: [],
+                                      filters: {
+                                type: 'all'
+                            }
+                   };
+                  }
+           fetchPets = () => {
+              let endpoint = '/api/pets';
+               if (this.state.filters.type !== 'all') {
+             endpoint += `?type=${this.state.filters.type}`;
       }
-      fetch(endpoint)
-        .then(res => res.json())
-  .then(pets => this.setState({ pets: pets }));
-    };
-    onChangeType = ({ target: { value } }) => {
-     this.setState({ filters: { ...this.state.filters, type: value } });
+           fetch(endpoint)
+                .then(res => res.json())
+  .         then(pets => this.setState({ pets: pets }));
+              };
+          onChangeType = ({ target: { value } }) => {
+                             this.setState({ filters: { ...this.state.filters, type: value } });
           };
              onAdoptPet = petId => {
-  const pets = this.state.pets.map(p => {
-   return p.id === petId ? { ...p, isAdopted: true } : p;
-      });
-     this.setState({ pets: pets });
+                         const pets = this.state.pets.map(p => {
+                       return p.id === petId ? { ...p, isAdopted: true } : p;
+                            });
+                         this.setState({ pets: pets });
                             };
-     render() {
-    return (
-    <div className="ui container"
-    <header>
+                         render() {
+              return (
+                 <div className="ui container">
+                              <header>
               <h1 className="ui dividing header">React Animal Shelter</h1>
                          </header>
-               <div className="ui container">
+                                         <div className="ui container">
                                                            <div className="ui grid">
                                                                     <div className="four wide column">
-<Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets}/>
-
-          </div>
+                                                              <Filters
+                                onChangeType={this.onChangeType}
+                                     onFindPetsClick={this.fetchPets}
+                                                                            />
+                                                             </div>
                                      <div className="twelve wide column">
                       <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
                         </div>
@@ -49,5 +51,5 @@ class App extends React.Component {
                                    </div>
                                                 )
                                                           }
-                                                        }
-export default App;
+                                                                      }
+                                                      export default App
